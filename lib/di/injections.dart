2dc7207781cdc974fed79ac1d3536a71cd8cf22c
Vyacheslav Ryabinin <sla.000@ya.ru>
2020-11-29 12:50:17 +0300

    Add injection

--- lib/di/injections.dart
+++ lib/di/injections.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_liga_stavok/logic/head_3_head/head_2_head_bloc.dart';
+import 'package:flutter_liga_stavok/logic/live_results/live_results_bloc.dart';
 import 'package:flutter_liga_stavok/logic/selected_event/selected_event_bloc.dart';
 import 'package:flutter_liga_stavok/logic/timer/timer_bloc.dart';
 import 'package:flutter_liga_stavok/logic/win_probability/win_probability_bloc.dart';
@@ -8,15 +9,21 @@ final GetIt getIt = GetIt.instance;
 
 void injectionsInit() {
   getIt.registerSingleton<SelectedEventBloc>(SelectedEventBloc());
+
   getIt.registerSingleton<WinProbabilityBloc>(WinProbabilityBloc());
   getIt.registerSingleton<Head2HeadBloc>(Head2HeadBloc());
+  getIt.registerSingleton<LiveResultsBloc>(LiveResultsBloc());
+
   getIt.registerSingleton<TimerBloc>(TimerBloc());
 }
 
 void injectionsDispose() {
   getIt.get<SelectedEventBloc>().dispose();
+
   getIt.get<WinProbabilityBloc>().dispose();
   getIt.get<Head2HeadBloc>().dispose();
+  getIt.get<LiveResultsBloc>().dispose();
+
   getIt.get<TimerBloc>().dispose();
 
   getIt.reset();
