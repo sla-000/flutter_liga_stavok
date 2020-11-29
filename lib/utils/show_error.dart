dfc571ebae54294cd193fcc3fb145d1d432ca9f6
Vyacheslav Ryabinin <sla.000@ya.ru>
2020-11-29 12:51:03 +0300

    Fixx showError

--- lib/utils/show_error.dart
+++ lib/utils/show_error.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_liga_stavok/utils/exception.dart';
 
 void showError(BuildContext context, Exception error) {
   Scaffold.of(context).showSnackBar(SnackBar(
     backgroundColor: Colors.red[200],
-    content: Text('Error: error=$error'),
+    content: Text('Error: ${AppException.getMessage(context, error)}'),
   ));
 }
