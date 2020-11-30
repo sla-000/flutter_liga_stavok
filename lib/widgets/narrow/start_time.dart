157a8871ebea4599d805251992b75c1eb390a61e
Vyacheslav Ryabinin <sla.000@ya.ru>
2020-11-30 21:41:51 +0300

    Add tournament name and match start

--- lib/widgets/narrow/start_time.dart
+++ lib/widgets/narrow/start_time.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_liga_stavok/di/injections.dart';
+import 'package:flutter_liga_stavok/logic/selected_event/selected_event_bloc.dart';
+import 'package:flutter_liga_stavok/rest/models/common.dart';
+import 'package:flutter_liga_stavok/utils/exception.dart';
+import 'package:flutter_liga_stavok/widgets/common/busy_widget.dart';
+import 'package:flutter_liga_stavok/widgets/common/fail_widget.dart';
+
+const double _kWidgetWidth = double.infinity;
+const double _kWidgetHeight = 16;
 
 class StartTime extends StatelessWidget {
   const StartTime({
@@ -7,6 +16,46 @@ class StartTime extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return Container(
+      width: _kWidgetWidth,
+      height: _kWidgetHeight,
+      child: Center(
+        child: Center(
+          child: StreamBuilder<SportEvent>(
+            stream: getIt.get<SelectedEventBloc>().stream,
+            builder:
+                (BuildContext context, AsyncSnapshot<SportEvent> snapshot) {
+              if (snapshot.hasError) {
+                if (snapshot.error is AppBusy) {
+                  return const BusyWidget(child: Center());
+                }
+
+                return FailWidget(
+                  error: snapshot.error.toString(),
+                  child: const Center(),
+                );
+              }
+
+              if (snapshot.hasData) {
+                return Text(
+                  getLocalTime(snapshot.data.scheduled),
+                  style: Theme.of(context).textTheme.headline4,
+                  overflow: TextOverflow.ellipsis,
+                );
+              }
+
+              return const Center();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  String getLocalTime(String time) {
+    final DateTime parsedTime = DateTime.parse(time);
+    final DateTime localTime = parsedTime.toLocal();
+
+    return '${localTime.day}.${localTime.month}.${localTime.year}, ${localTime.hour}:${localTime.minute}';
   }
 }
--- lib/widgets/narrow/tournament_name.dart
+++ lib/widgets/narrow/tournament_name.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_liga_stavok/di/injections.dart';
+import 'package:flutter_liga_stavok/logic/selected_event/selected_event_bloc.dart';
+import 'package:flutter_liga_stavok/rest/models/common.dart';
+import 'package:flutter_liga_stavok/utils/exception.dart';
+import 'package:flutter_liga_stavok/widgets/common/busy_widget.dart';
+import 'package:flutter_liga_stavok/widgets/common/fail_widget.dart';
+
+const double _kWidgetWidth = double.infinity;
+const double _kWidgetHeight = 24;
 
 class TournamentName extends StatelessWidget {
   const TournamentName({
@@ -8,14 +17,38 @@ class TournamentName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 30,
+      width: _kWidgetWidth,
+      height: _kWidgetHeight,
       child: Center(
-          child: Text(
-        'Tournament name',
-        style: Theme.of(context).textTheme.headline2,
-        overflow: TextOverflow.ellipsis,
-      )),
+        child: Center(
+          child: StreamBuilder<SportEvent>(
+            stream: getIt.get<SelectedEventBloc>().stream,
+            builder:
+                (BuildContext context, AsyncSnapshot<SportEvent> snapshot) {
+              if (snapshot.hasError) {
+                if (snapshot.error is AppBusy) {
+                  return const BusyWidget(child: Center());
+                }
+
+                return FailWidget(
+                  error: snapshot.error.toString(),
+                  child: const Center(),
+                );
+              }
+
+              if (snapshot.hasData) {
+                return Text(
+                  snapshot.data.season?.name ?? 'Season',
+                  style: Theme.of(context).textTheme.headline2,
+                  overflow: TextOverflow.ellipsis,
+                );
+              }
+
+              return const Center();
+            },
+          ),
+        ),
+      ),
     );
   }
 }
