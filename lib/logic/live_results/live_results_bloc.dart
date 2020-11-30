81d1c2cc7f480d9a50622ebb58dfdef8617e25f9
Vyacheslav Ryabinin <sla.000@ya.ru>
2020-11-30 20:20:58 +0300

    Update live events with timer

--- lib/logic/live_results/live_results_bloc.dart
+++ lib/logic/live_results/live_results_bloc.dart
@@ -4,19 +4,24 @@ import 'package:flutter_liga_stavok/di/injections.dart';
 import 'package:flutter_liga_stavok/logic/exceptions.dart';
 import 'package:flutter_liga_stavok/logic/selected_event/selected_event_bloc.dart';
 import 'package:flutter_liga_stavok/logic/subscribe_bloc.dart';
+import 'package:flutter_liga_stavok/logic/timer/timer_bloc.dart';
 import 'package:flutter_liga_stavok/rest/api/requests.dart';
 import 'package:flutter_liga_stavok/rest/models/common.dart';
 import 'package:flutter_liga_stavok/rest/models/live_results.dart'
     as live_results;
 import 'package:logging/logging.dart';
+import 'package:rxdart/rxdart.dart';
 
 final Logger _log = Logger('LiveResultsBloc');
 
 class LiveResultsBloc extends SubscribeBloc<SportEvent, Result> {
   @override
   StreamSubscription<SportEvent> subscribe() {
-    return getIt.get<SelectedEventBloc>().stream.listen(
-        (SportEvent sportEvent) async {
+    return CombineLatestStream.combine2<SportEvent, void, SportEvent>(
+      getIt.get<SelectedEventBloc>().stream,
+      getIt.get<TimerBloc>().stream,
+      (SportEvent sportEvent, _) => sportEvent,
+    ).listen((SportEvent sportEvent) async {
       _log.finest(() => 'subscribe: sportEvent=$sportEvent');
       if ((sportEvent?.competitors?.length ?? 0) != 2) {
         clear();
