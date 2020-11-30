7a11b82450fdce1e645aae723e6e71f210c3df8c
Vyacheslav Ryabinin <sla.000@ya.ru>
2020-12-01 00:21:28 +0300

    Add update indicator

---
+++ lib/widgets/common/loading_indicator.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_liga_stavok/di/injections.dart';
+import 'package:flutter_liga_stavok/logic/live_results/live_results_bloc.dart';
+import 'package:flutter_liga_stavok/rest/models/common.dart';
+import 'package:flutter_liga_stavok/utils/exception.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Result>(
+      stream: getIt.get<LiveResultsBloc>().stream,
+      builder: (BuildContext context, AsyncSnapshot<Result> snapshot) {
+        if (snapshot.hasError) {
+          if (snapshot.error is AppBusy) {
+            return const CircularProgressIndicator();
+          }
+
+          return FittedBox(
+            child: Icon(
+              Icons.warning,
+              color: Colors.red[400],
+            ),
+            fit: BoxFit.contain,
+          );
+        }
+
+        return const Center();
+      },
+    );
+  }
+}
--- lib/widgets/narrow/liga_stavok.dart
+++ lib/widgets/narrow/liga_stavok.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_liga_stavok/widgets/common/loading_indicator.dart';
 import 'package:flutter_liga_stavok/widgets/narrow/match_score/match_score.dart';
 import 'package:flutter_liga_stavok/widgets/narrow/match_time.dart';
 import 'package:flutter_liga_stavok/widgets/narrow/start_time.dart';
@@ -12,27 +13,40 @@ class LigaStavokNarrow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        const TournamentName(),
-        const SizedBox(height: 2),
-        const StartTime(),
-        const SizedBox(height: 8),
-        const MatchScore(),
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          children: const <Widget>[
-            Flexible(
-              flex: 30,
-              child: MatchTime(),
-            ),
-            Flexible(
-              flex: 70,
-              child: TeamsChances(),
+    return Stack(
+      children: [
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const TournamentName(),
+            const SizedBox(height: 2),
+            const StartTime(),
+            const SizedBox(height: 8),
+            const MatchScore(),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: const <Widget>[
+                Flexible(
+                  flex: 30,
+                  child: MatchTime(),
+                ),
+                Flexible(
+                  flex: 70,
+                  child: TeamsChances(),
+                ),
+              ],
             ),
           ],
+        ),
+        const Positioned(
+          top: 5,
+          left: 5,
+          child: SizedBox(
+            width: 16,
+            height: 16,
+            child: LoadingIndicator(),
+          ),
         ),
       ],
     );
